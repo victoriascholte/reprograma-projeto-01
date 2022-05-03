54cd244d05f8cbc2aa3f349c22d3c86d65fff903
Victoria Scholte <victoria.scholte@gmail.com>
2022-05-03 18:45:18 -0300

    update listarRecomendados.js

--- controllers/listarRecomendados.js
+++ controllers/listarRecomendados.js
@@ -1,7 +1,7 @@
 const livros = require('../database.js')
 const read = require('readline-sync')
 const listarRecomendados = () => {
-    const confirmRecomendados = read.question('Deseja visualizar os livros recomendados? S/N ').toUpperCase()
+    const confirmRecomendados = read.question('Deseja visualizar os livros recomendados? S/N').toUpperCase()
     if (confirmRecomendados === 'S') {
             const livrosRecomendados = livros.filter (livro => livro.recomenda === true)
             console.table(livrosRecomendados)        
