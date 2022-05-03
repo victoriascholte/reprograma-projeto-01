b1493f26cb27a24a7720c2f89ab665894486889a
Victoria Scholte <victoria.scholte@gmail.com>
2022-05-03 18:44:27 -0300

    update listarLivrosNaoLidos.js

--- controllers/listarLivrosNaoLidos.js
+++ controllers/listarLivrosNaoLidos.js
@@ -1,7 +1,7 @@
 const livros = require('../database.js')
 const read = require('readline-sync')
 const listarLivrosNaoLidos = () => {
-    const confirmNaoLidos = read.question('Deseja visualizar os livros não lidos? S/N ').toUpperCase()
+    const confirmNaoLidos = read.question('Deseja visualizar os livros não lidos? S/N').toUpperCase()
     if (confirmNaoLidos === 'S') {
         const livrosNaoLidos = livros.filter (livro => livro.leu === false)
         console.table(livrosNaoLidos)
