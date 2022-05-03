8ba7df83daf967f1d92c9bf08dbba7e43120bffd
Victoria Scholte <victoria.scholte@gmail.com>
2022-05-03 18:43:45 -0300

    Update buscarLivros.js

--- controllers/buscarLivros.js
+++ controllers/buscarLivros.js
@@ -1,11 +1,11 @@
 const livros = require('../database.js')
 const read = require('readline-sync')
 const buscarLivros = () => {
-    const opcaoInicial = read.question('Deseja buscar por categoria? S/N ').toUpperCase()
+    const opcaoInicial = read.question('Deseja buscar por categoria? S/N').toUpperCase()
     if (opcaoInicial === 'S') {
        const categorias = livros.map(livro => livro.categoria)
        console.log(categorias)
-       const inputCategoria =read.question('Digite uma categoria, conforme tabela acima. ')
+       const inputCategoria =read.question('Digite uma categoria, conforme tabela acima.')
        const confirmCategoria = read.question('Confirma? S/N').toUpperCase()
        if (confirmCategoria === 'S'){
            const livrosFiltrados = livros.filter(livro => livro.categoria === inputCategoria)
